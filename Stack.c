59153a9d7fd46197570d9934907e83d714db8990
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2021-10-22 09:18:53 +0530

    Final Program Stack

--- Stack.c
+++ Stack.c
@@ -8,12 +8,37 @@ int push(int data)
 	stack[top] = data;
 	return 1;
 }
-void display()
+void peep()
 {
-	for(int i=0;i<top;i++)
+	for(int x = 5;x>top;x--)
 	{
-		printf("\n\t Stack[%d] = %d",i,stack[i]);		
-	}	
+		printf("\n\t|    |");
+		printf("\n\t|____|");
+		if(x == top-1)
+		{
+			printf("<--top");		
+		}
+	}			
+	for(int i=top;i>=0;i--)
+	{
+		printf("\n\t| %d |",stack[i]);	
+		printf("\n\t|____|");	
+		if(i == top)
+		{
+			printf("<--top");		
+		}		
+	}
+}
+int pop()
+{
+	top = top-1;	
+	return 1;
+}
+
+int updatation(int update,int value)
+{
+	stack[update] = value;
+	return 1;
 }
 int isEmpty()
 {
@@ -37,15 +62,18 @@ int isOverflow()
 		return 0; 	
 	}
 }
+
+
+
+
 int main()
 {
-	int ch,sum,data;
+	int ch,sum,data,update,value;
 	do{
 		printf("\n\n\t 1.Push");
 		printf("\n\t 2.Peep");
 		printf("\n\t 3.Pop");
 		printf("\n\t 4.Update");
-		printf("\n\t 5.Display");
 		printf("\n\t 0.Exit");
 
 		printf("\n\n\t Select Your Choice : ");
@@ -63,26 +91,73 @@ int main()
 				{	
 					printf("\n\t Enter The Data : ");
 					scanf("%d",&data);
-					printf("%d",push(data));
+
 					if(push(data) == 1)
 					{
 						printf("\n\t Data inserted Successfully");
 					}
 					
 				}
-				
-			break;
+				break;
 			case 2:
-				printf("\n\tPeep");
-			break;
+				if(isEmpty() == 0)
+				{
+					peep();
+		
+				}
+				else
+				{
+					for(int i=0;i<(5-top);i++)
+					{
+						printf("\n\t|-----|");
+						if(i == 5)
+						{
+							printf("<--top");		
+						}
+				
+					}	
+					printf("\n\t Stack is Empty");
+					break;
+				}
+				break;
+
 			case 3:
-				printf("\n\tPop");
+				if(isEmpty() == 0)
+				{
+					int sum = pop();
+					if(sum == 1)
+					{
+						printf("\n\t Data Deleted Successfully");	
+					}		
+				}
+				else
+				{
+					printf("\n\t Stack is Empty");
+					break;
+				}
 			break;
 			case 4:
-				printf("\n\tUpdate");
-			break;
-			case 5:
-				display();
+				
+				printf("\n\tEnter The stack position to update : ");
+				scanf("%d",&update);
+				if(update > top || update < -1)
+				{
+					printf("\n\t Please Enter Proper Index");
+				}
+				else
+				{
+					printf("\n\tEnter The Value For Updation : ");
+					scanf("%d",&value);
+					int up = updatation(update,value);
+					if(up == 1)
+					{
+						printf("\n\tStack Updated Successfully");
+					}
+					else
+					{
+						printf("\n\tSomething Went wrong");	
+					}
+				}
 			break;
 			case 0:
 				printf("\n\tbye bye\n\n");
