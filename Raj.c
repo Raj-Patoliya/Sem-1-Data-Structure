8765677556adb37b9d3512636c1e4bda31560768
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2021-10-25 13:12:52 +0530

    Update Raj.c

--- Raj.c
+++ Raj.c
@@ -1,43 +1,57 @@
 #include<stdio.h>
 int main()
 {
-    int num,temp,arr[4][4];
-    int row[4],col[4];
-    printf("\n\t Enter The Numbers of Vertices : ");
-    scanf("%d", &num);
-    for (int x = 1; x <= num; x++)
+	int num,temp,edge;
+	printf("\n\t Enter The Numbers of Vertices : ");	    
+	scanf("%d", &num);
+
+	do
+	{
+		printf("\n\t Enter The Numbers of Edges : ");
+    		scanf("%d", &edge);
+	
+		if(edge > (num*num) || edge <= 0)
+		{
+			printf("\n\t :: Please Enter Proper Edges::\n");
+		}	
+	}
+	while(edge > (num*num) || edge < 0);
+	int arr[num][num];
+	int row[num],col[num];
+
+   	for (int x = 0; x < edge; x++)
+    	{
+        	printf("\n\t Edge No %d \'s Row : ",x);
+        	scanf("%d", &row[x]);
+        	printf("\n\t Edge No %d \'s Column : ",x);
+        	scanf("%d", &col[x]);
+    	}
+    for (int x = 0; x < num; x++)
     {
-        printf("\n\t Vertices No %d \'s Row : ",x);
-        scanf("%d", &row[x]);
-        printf("\n\t Vertices No %d \'s Column : ",x);
-        scanf("%d", &col[x]);
-    }
-    for (int x = 1; x <= num; x++)
-    {
-        
-        for (int i = 1; i <= num; i++)
+       
+        for (int i = 0; i < num; i++)
         {     
             arr[x][i] = 0 ;
         }
     }
-    for (int x = 1; x <= num; x++)
+    for (int x = 0; x < edge; x++)
     {
         
         arr[row[x]][col[x]] = 1;
     }
 
         
-        for (int i = 1; i <= num; i++)
+        for (int i = 0; i < num; i++)
         {     
-            printf("\n\t");     
-            for (int j = 1; j <= num; j++)
+            printf("\n\t ");     
+            for (int j = 0; j < num; j++)
             {
                 printf(" %d ",arr[i][j]);    
             }
             
         }
     
-    for (int s = 1; s <= num; s++)
+    for (int s = 0; s < edge; s++)
     {
         printf("\n\t (%d  %d ) ",row[s],col[s]);
     }
