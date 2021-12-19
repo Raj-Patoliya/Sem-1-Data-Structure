c2541923cbe73a5d7554f8e24bcf24aba076518d
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2021-12-20 03:12:26 +0530

    Update Selection Sort.c

--- Selection Sort.c
+++ Selection Sort.c
@@ -57,3 +57,31 @@ void main()
          printf("\t%d",arr[i]);
     }    
 }
+
+/*Insertion Sort*/
+
+#include <stdio.h>  
+int main(){  
+    int a[6] = { 12, 31, 25, 8, 32, 17 };  
+    int n = 6 ;  
+    int j, temp;
+    printf("\n\n\tBefore sorting array elements are - \n\n");  
+    for (int i = 0; i < n; i++) 
+    { 
+        printf("%d ", a[i]);  
+    }   
+    for (int i = 1; i < n; i++) {  
+        temp = a[i];  
+        j = i - 1;  
+        while(j>=0 && temp <= a[j])  
+        {    
+            a[j+1] = a[j];     
+            j = j-1;    
+        }    
+        a[j+1] = temp;    
+    }
+    printf("\n\n\tAfter sorting array elements are - \n\n");    
+    for (int i = 0; i < n; i++) {
+            printf(" %d ", a[i]);
+        }  
+}    
