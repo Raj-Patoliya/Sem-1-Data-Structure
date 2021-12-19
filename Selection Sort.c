5db713e431fb37bd03025b759391c595e2d43f0f
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2021-12-20 02:13:20 +0530

    Update Selection Sort.c

--- Selection Sort.c
+++ Selection Sort.c
@@ -1,3 +1,5 @@
+
+/*Selection Sort*/
 #include<stdio.h>
 void main()
 {
@@ -23,4 +25,35 @@ void main()
     for (int i = 0; i <size; i++){
          printf("\t%d",arr[i]);
     }
+}
+
+/*Bubble Sort*/
+#include<stdio.h>
+void main()
+{
+    int size;
+    printf("\n\t Enter The Size Of Array  : ");
+    scanf("%d",&size);
+    int arr[size],temp;
+    for (int i = 0; i < size; i++)
+    {
+        printf("\n\tArray [%d] = ",i);
+        scanf("%d",&arr[i]);    
+    }
+    for(int i = 0;i<size;i++)
+    {
+        for(int j = 0;j<=i;j++)
+        {
+            if(arr[i] < arr[j])
+            {
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+    for (int i = 0; i <size; i++)
+    {
+         printf("\t%d",arr[i]);
+    }    
 }
