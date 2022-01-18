bc82ce5e031da30a4135b7ae68546f8550d457bf
Raj Patoliya <84698326+iamrjpeace@users.noreply.github.com>
2022-01-18 15:45:40 +0530

    Update Sorting Programs.c

--- Sorting Programs.c
+++ Sorting Programs.c
@@ -130,3 +130,45 @@ int main() {
     printf("\n");
     return 0;
 }
+
+/* Merge Sort */
+
+#include<stdio.h>
+#include<stdlib.h>
+void merge(int a[],int low,int mid ,int high)
+{
+	int b[20];
+	int i,j,k;
+	i=low,j=mid+1,k=low;
+	while(i<=mid && j<=high){
+		if(a[i]<=a[j])
+		    b[k++]=a[i++];
+		else
+		   b[k++]=a[j++]; 
+	}
+	while (i<=mid)
+		b[k++]=a[i++];
+	while 
+		(j<=high) b[k++]=a[j++];
+		for (k=low;k<=high;k++)
+	        a[k]=b[k];
+}
+void mergesort(int a[],int low,int high){
+	int mid;
+	if(low>=high)
+	  return;
+	mid=(low+high)/2;
+	mergesort(a,low,mid);
+	mergesort(a,mid+1,high);
+	merge(a,low,mid,high);
+}
+int main() {
+	int a[7] = {83, 20, 9, 50, 115, 61, 17};
+	int n = 7;
+	mergesort(a, 0, n-1);
+	printf("\n Sorted numbers are : \n");
+	for(int k=0;k<7;k++)
+	    printf("%d\t",a[k]);
+	return 0;
+}
+
